ea262d3efc582188b46f1e807acc6bf8c69993d9
tizenegy <bastian.elfert@outlook.com>
2020-10-15 16:02:50 +0300

    added CSRF token, Django forms, POST action, handling of POST, and redirect.

--- wiki/tasks/views.py
+++ wiki/tasks/views.py
@@ -1,13 +1,27 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 from datetime import datetime
+from django import forms
 
 # Create your views here.
 
 tasks =["foo", "bar", "baz"]
 
+class NewTaskForm(forms.Form):
+    task = forms.CharField(label="New Task")
+    # priority = forms.IntegerField(label="Priority", min_value=1, max_value=5)
+
 def index(request):
     return render(request, "tasks/index.html", {"tasks": tasks})
 
 def add(request):
-    return render(request, "tasks/add.html")
+    if request.method == "POST":
+        form = NewTaskForm(request.POST)
+        if form.is_valid():
+            task = form.cleaned_data["task"]
+            tasks.append(task)
+            return HttpResponseRedirect(reverse("tasks:index"))
+        else:
+            return render(request, "tasks/add.html", {"form": form})
+    return render(request, "tasks/add.html", {"form": NewTaskForm()})
