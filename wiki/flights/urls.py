bd77a0056627b3aa72be8e83f3c5480c2d8904a0
Bastian Elfert <bastian.elfert@outlook.com>
2020-10-20 19:50:38 +0300

    added passengers in n:n rel to flights. added forms on flight-page that can be used to add and remove passengers to/from flights. both do this with post requests to the new route /flights/[flightnumber]/book (or remove). added number of currently booked passengers to flights on index.

--- wiki/flights/urls.py
+++ wiki/flights/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from . import views
 
-# app_name = "flights"
+app_name = "flights"
 urlpatterns = [
     path("", views.index, name="index"),
     path("<int:flight_id>", views.flight, name="flight"),
+    path("<int:flight_id>/book", views.book, name="book"),
+    path("<int:flight_id>/remove", views.remove, name="remove")
 ]
--- wiki/flights/views.py
+++ wiki/flights/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-from .models import Flight
+from .models import *
+from django.http import HttpResponseRedirect
+from django.urls import reverse
 # Create your views here.
 
 def index(request):
@@ -8,7 +10,23 @@ def index(request):
     })
 
 def flight(request, flight_id):
-    flight = Flight.objects.get(id=flight_id)
+    flight = Flight.objects.get(pk=flight_id)
     return render(request, "flights/flight.html", {
-        "flight": flight
+        "flight": flight,
+        "passengers": flight.passengers.all(),
+        "non_passengers": Passenger.objects.exclude(flights=flight).all()
     })
+
+def book(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.add(flight)
+    return HttpResponseRedirect(reverse("flights:flight", args=(flight.id,)))
+
+def remove(request, flight_id):
+    if request.method == "POST":
+        flight = Flight.objects.get(pk=flight_id)
+        passenger = Passenger.objects.get(pk=int(request.POST["passenger"]))
+        passenger.flights.remove(flight)
+    return HttpResponseRedirect(reverse("flights:flight", args=(flight.id,)))
