d4c21cb34ce511d61b3b1017bda7098d516e9dd8
Bastian Elfert <bastian.elfert@outlook.com>
2020-10-20 20:57:20 +0300

    created new app "users" and build urls, views, templates. implemented rudimentary user management. todo: logout.

--- wiki/wiki/settings.py
+++ wiki/wiki/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'hello',
     'flights',
+    'users',
     'tasks',
     'newyear',
     'django.contrib.admin',
--- wiki/wiki/urls.py
+++ wiki/wiki/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('hello/', include('hello.urls')),
     path('flights/', include('flights.urls')),
+    path('users/', include('users.urls')),
     path('tasks/', include('tasks.urls')),
     path('admin/', admin.site.urls),
     path('newyear/', include('newyear.urls')),
